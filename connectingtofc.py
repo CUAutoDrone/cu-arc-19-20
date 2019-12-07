906d539acc18bf74559f06eee437f116f7b85772
nicholaskgeorge <nkg37@cornell.edu>
2019-12-07 16:58:29 -0500

    First test of connection

--- connectingtofc.py
+++ connectingtofc.py
@@ -1,26 +1,41 @@
 import serial
 
 def connecttoport(dport):
+"""This function connect to the desired port"""
     port= serial.Serial(dport,15200, timeout=1,write_timeout=1)
     print("Desired port is "+port.name)
     return port
+
+
 def send(dmsg, port):
+"""This function is what is used to send the final message. It takes the port
+    you want and the message as arguments."""
     port.write(dmsg)
+
+
 def pack(channels):
+"""This function packs the desired message in the ibus format. You give it the
+    values for all 14 channel in an array. Unused channels must be given the
+    value 0x05DC"""
     message=[]
     message.append(0x20)
     message.append(0x40)
     for channel in channels:
         message.append(channel%256)
         message.append(channel//256)
-    #begins the sum needed to create the checksum bytes Not use if this should be
-    #0x4020 instead
     msgsum=0
     for i in message:
         msgsum+=i
     cheksum = 0xfff-msgsum
-    message.appned(checksum%256)
-    message.appned(checksum//256)
+    message.append(checksum%256)
+    message.append(checksum//256)
+    return message
 
 def test():
-    pass
+    channel = [1000]*14
+    port=connecttoport('ttyAMA0')
+    message = pack(channel)
+    send(message, port)
+
+if __name__ == "__main__":
+    test()
