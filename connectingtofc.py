23a6bb2d49a062acb7c23ff687894ddc295e87ee
nicholaskgeorge <nkg37@cornell.edu>
2019-12-06 22:33:55 -0500

    changed the sum. Idk if its right

--- connectingtofc.py
+++ connectingtofc.py
@@ -13,11 +13,14 @@ def pack(channels):
     for channel in channels:
         message.append(channel%256)
         message.append(channel//256)
-    msgsum=0x4020
+    #begins the sum needed to create the checksum bytes Not use if this should be
+    #0x4020 instead
+    msgsum=0x2040
+    #idk if this sum is correct
     for i in message:
         msgsum+=i
     cheksum = 0xfff-msgsum
-    message.appned(checksum%246)
+    message.appned(checksum%256)
     message.appned(checksum//256)
 
 def test():
