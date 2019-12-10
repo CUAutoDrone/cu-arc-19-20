30f5ca372d0f965fe0475da4989634a387bed5d7
red1 <CUAerialRobotics@gmail.com>
2019-12-10 23:35:16 +0000

    We now have a sucessful connection to the flight controller

--- connectingtofc.py
+++ connectingtofc.py
@@ -4,7 +4,7 @@ import struct
 
 def connecttoport(dport):
     """This function connect to the desired port"""
-    port= serial.Serial(dport,15200, timeout=10, write_timeout=10)
+    port= serial.Serial(dport,115200, timeout=10, write_timeout=10)
     print("Desired port is "+port.name)
     return port
 
@@ -28,21 +28,29 @@ def pack(channels):
     msgsum=0
     for i in message:
         msgsum+=i
-    checksum = 0xfff-msgsum
+    checksum = 0xffff-msgsum
     message.append(checksum%256)
     message.append(checksum//256)
 
     return list(map(lambda i :struct.pack(b'B',i),message))
-
-def test():
-    port = serial.Serial('/dev/ttyAMA0',15200)
-    port.write(128)
-    for i in range(2000):
-        channel = [1000]*14
-        port=connecttoport('/dev/ttyAMA0')
-        message = pack(channel)
+def commands(channels):
+    command = []
+    for i in channels:
+        command.append(i)
+    command+=([0x05dc]*(14-len(channels)))
+    message = pack(command)
+    with connecttoport('/dev/ttyS0') as port:
         send(message, port)
-        sleep(10)
-
+def test():
+    commands([1000]*4)
+    # print("start")
+    # port = serial.Serial('/dev/ttyS0',115200, timeout=10, write_timeout=10 )
+    # port.write(struct.pack(b'B',128))
+    # print("end")
+    # port=connecttoport('/dev/ttyS0')
+    # channel = [1000]*14
+    # message = pack(channel)
+    # for i in range(2000):
+    #     send(message, port)
 if __name__ == "__main__":
     test()
