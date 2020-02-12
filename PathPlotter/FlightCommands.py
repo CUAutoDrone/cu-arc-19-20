02b2dfd0768b200e6cd45533d48ea30fd1f1ca84
nicholaskgeorge <nkg37@cornell.edu>
2020-02-11 22:52:03 -0500

    Finished commands for movment. We wanted to have a publisher subscriber model for the threads. Im not sure if the code full achieves that but we can decide that in our next meeting

--- PathPlotter/FlightCommands.py
+++ PathPlotter/FlightCommands.py
@@ -1,6 +1,10 @@
 import connectingtofc
 import threading
 import time
+"""this library allows you to send messages between threads. We are trying to
+   avoid the threads looking at the same variable in in memory so this miight be
+   a solution"""
+from queue import Queue
 
 """This file sets up the basic direction commands which will be needed to move
    the drone. Creates a thread which constantly sends signals to the Flight
@@ -13,29 +17,44 @@ pitch = 1500
 yaw = 1500
 throttle = 885
 arming = 1500
+messanger = Queue()
+
 def constantsend():
     """sends the desired values to the FC every 0.01 secconds"""
     while not kill:
+        "reads desired values to send"
+        recieved = messanger.get()
+        for i in recieved:
+            if i == 'roll':
+                roll = i[1]
+            elif i == 'pitch':
+                pitch = i[1]
+            elif i == 'yaw':
+                yaw = i[1]
+            elif i == 'throttle':
+                throttle = i[1]
+            elif i == 'arm':
+                arm = 1900
+            else:
+                arm = 1500
         message=[roll,pitch,yaw,throttle,1500,arm]
         commands(message)
         time.sleep(0.01)
+    print("Ending sending thread")
         #add code which complies with messages from flight commands
 
 def roll(value):
-    #code that aks thread to change roll
-    pass
+    messanger.put(['roll',value])
 def pitch(value):
-    #code that asks thread to change pitch
-    pass
+    messanger.put(['pitch',value])
 def yaw(value):
-    #code that tells thread to changes yaw
-    pass
+    messanger.put(['yaw',value])
 def throttle(value):
-    #put code that tells thread to change throttle
-    pass
+    messanger.put(['throttle',value])
 def arm():
-    #code that arms
-    pass
+    messanger.put(['arm'])
+def dissarm():
+    messanger.put(["dissarm"])
 def stopsend():
     """function which stops the thread if needed"""
     kill = True
